57423caa6916fa746d03d02bdf23a81b29c519e9
melissa <melissa.iman.n@uni.pe>
2024-11-14 10:24:50 -0500

    fix: remove unused variables and imports

--- src/components/productCard/__test__/ProductCart.test.tsx
+++ src/components/productCard/__test__/ProductCart.test.tsx
@@ -1,5 +1,4 @@
 import '@testing-library/jest-dom';
-import React from 'react';
 import { render, screen, fireEvent } from '@testing-library/react';
 import ProductCard from '../ProductCard';
 import { ProductCardProps } from '../productCard.domain';
--- src/components/select/Select.tsx
+++ src/components/select/Select.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from 'react';
+import { FC } from 'react';
 import styles from './Select.module.css';
 
 interface SelectOption {
